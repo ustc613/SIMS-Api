b894e46e771842cfb8bc2c4c8323d1290877f07a
nvcooc <985116196@qq.com>
2021-12-21 16:31:26 +0800

    init project

---
+++ src/main/java/com/simsapi/SimsApiApplication.java
@@ -0,0 +1,13 @@
+package com.simsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simsapi/SimsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
