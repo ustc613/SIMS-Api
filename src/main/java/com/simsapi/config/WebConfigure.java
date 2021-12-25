c184ccac2d80dc8818ff8f70699c55350e712335
SUDOCS <admin@imhcl.cn>
2021-12-25 13:40:02 +0800

    全局跨域配置

--- src/main/java/com/simsapi/config/WebConfigure.java
+++ src/main/java/com/simsapi/config/WebConfigure.java
@@ -2,15 +2,27 @@ package com.simsapi.config;
 
 import cn.dev33.satoken.interceptor.SaAnnotationInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SaTokenConfigure implements WebMvcConfigurer {
+public class WebConfigure implements WebMvcConfigurer {
     // 注册Sa-Token的注解拦截器，打开注解式鉴权功能
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册注解拦截器，并排除不需要注解鉴权的接口地址 (与登录拦截器无关)
         registry.addInterceptor(new SaAnnotationInterceptor()).addPathPatterns("/**");
+    }
+
+    // 全局跨域配置
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
     }
 }
